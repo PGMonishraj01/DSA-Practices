23f3a8f75d98f08d86f3292247ec7a43024e6484
Monishraj P G <pgmonishraj01@gmail.com>
2026-03-09 21:17:59 +0530

    Added Indexes of Subarray Sum solution

---
+++ GFG/Indexes of Subarray Sum.java
@@ -0,0 +1,9 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                if(arr[i]+arr[j]==target)
+            }
+        }
+    }
+}
