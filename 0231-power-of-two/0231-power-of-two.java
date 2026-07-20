eebac6cf05fabad7dd95e7a68eabda359ce5fc4e
Monishraj P G <pgmonishraj01@gmail.com>
2026-07-20 15:37:17 +0530

    Time: 6 ms (0.79%), Space: 42.9 MB (5.04%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for(int i =0 ;i <= Math.sqrt(n)+1 ; i++){
+            if(Math.pow(2 ,i)== n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
