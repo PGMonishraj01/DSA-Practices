c62d632fa1ac9631ea8be16fa9827bcbd07c8019
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-04 12:30:27 +0530

    Time: 3 ms (88.63%), Space: 43.1 MB (67.15%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -1,26 +1,31 @@
 class Solution {
-
     public boolean canPartitionKSubsets(int[] nums, int k) {
 
-        int total = 0;
-        for (int num : nums) total += num;
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-        if (total % k != 0) return false;
+        if (sum % k != 0) return false;
 
-        int target = total / k;
+        int target = sum / k;
 
         Arrays.sort(nums);
+
+        // Largest number cannot fit in a bucket
+        if (nums[nums.length - 1] > target) return false;
 
         boolean[] used = new boolean[nums.length];
 
         return backtrack(nums, used, k, 0, 0, target);
     }
 
-    public boolean backtrack(int[] nums, boolean[] used, int k, int start, int sum, int target) {
+    public boolean backtrack(int[] nums, boolean[] used, int k,
+                             int start, int currSum, int target) {
 
+        // Last bucket will automatically be correct
         if (k == 1) return true;
 
-        if (sum == target) {
+        // Current bucket completed
+        if (currSum == target) {
             return backtrack(nums, used, k - 1, 0, 0, target);
         }
 
@@ -28,14 +33,23 @@ class Solution {
 
             if (used[i]) continue;
 
-            if (sum + nums[i] > target) continue;
+            if (currSum + nums[i] > target) continue;
 
             used[i] = true;
 
-            if (backtrack(nums, used, k, i + 1, sum + nums[i], target))
+            if (backtrack(nums, used, k, i + 1,
+                          currSum + nums[i], target))
                 return true;
 
             used[i] = false;
+
+            // Optimization 1
+            if (currSum == 0) return false;
+
+            // Optimization 2
+            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                i++;
+            }
         }
 
         return false;
