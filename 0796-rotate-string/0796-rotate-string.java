b01afea4817422fc5d1e115cacdec069fded2900
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-06 15:57:09 +0530

    Time: 1 ms (78.85%), Space: 43.2 MB (64.65%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,7 +1,26 @@
 class Solution {
+    public static char[] reverse(char ch[], int start ,int end){
+        while(start < end){
+            char temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+            start++;
+            end--;
+        }
+        return ch;
+    }
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length())return false;
-        s = s+s;
-        return s.contains(goal);
+        char ch[] = s.toCharArray();
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            int  k = 1;
+            k = k%n;
+            reverse(ch,0,k-1);
+            reverse(ch,k,n-1);
+            reverse(ch,0,n-1);
+            s = new String(ch);
+            if(s.equals(goal))return true;
+        }
+        return false;
     }
 }
