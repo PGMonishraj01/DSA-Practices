f28b9053cecafe582e04f119639ba2d3a2eff151
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-07 16:18:38 +0530

    Time: 7 ms (2.14%), Space: 46.8 MB (5.34%) - LeetHub

---
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasTrailingZeros(int[] nums) {
+        for(int i = 0; i <nums.length; i++){
+            for(int j =i+1;j<nums.length;j++){
+                String s = Integer.toBinaryString(nums[i] | nums[j]);
+                if(s.charAt(s.length()-1) == '0')return true;
+            }
+        }
+        return false;
+    }
+}
