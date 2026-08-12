4ef7d401dffb84b57eed74a7b46e8decf8d19643
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-12 10:20:56 +0530

    Time: 15 ms (20.32%), Space: 48.1 MB (6.07%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -4,17 +4,21 @@ class Solution {
                 .mapToObj(String::valueOf)
                 .collect(Collectors.toList());
 
-        List<String> original = new ArrayList<>(list);
-
         int[] arr = Arrays.copyOf(score, score.length);
         Arrays.sort(arr);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < score.length; i++) {
+            map.put(score[i], i);
+        }
 
         int c = 0;
 
         for (int i = arr.length - 1; i >= 0; i--) {
             c++;
 
-            int index = original.indexOf(String.valueOf(arr[i]));
+            int index = map.get(arr[i]);
 
             if (c == 1) {
                 list.set(index, "Gold Medal");
