1c64f1ca77a438202aae01056596cc92bf1f0f1c
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-12 10:19:02 +0530

    Time: 293 ms (5.03%), Space: 48.4 MB (6.07%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        List<String> list = Arrays.stream(score)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList());
+
+        List<String> original = new ArrayList<>(list);
+
+        int[] arr = Arrays.copyOf(score, score.length);
+        Arrays.sort(arr);
+
+        int c = 0;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            c++;
+
+            int index = original.indexOf(String.valueOf(arr[i]));
+
+            if (c == 1) {
+                list.set(index, "Gold Medal");
+            } else if (c == 2) {
+                list.set(index, "Silver Medal");
+            } else if (c == 3) {
+                list.set(index, "Bronze Medal");
+            } else {
+                list.set(index, String.valueOf(c));
+            }
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
