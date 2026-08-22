7d053ee5d49815bae127a8110715ad14e5393857
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-22 15:41:12 +0530

    Time: 1417 ms (5.08%), Space: 48.9 MB (80.63%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -3,7 +3,7 @@ class Solution {
         for(int i = 0; i < arr.length - 1; i++){
             int max = 0;
             for(int j = i+1; j < arr.length; j++){
-                max = Math.max(max, arr[j]);
+                if(arr[j]> max)max = arr[j];
             }
             arr[i] = max;
         }
