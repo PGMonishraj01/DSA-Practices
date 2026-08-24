4427ec95c83645f1a7ff92f9701403948c87300b
Monishraj P G <pgmonishraj01@gmail.com>
2026-08-24 10:39:42 +0530

    Time: 11 ms (5.71%), Space: 45.7 MB (6.77%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingMultiple(int[] nums, int k) {
         Arrays.sort(nums);
         List<Integer> list = Arrays.stream(nums).boxed().toList();;
-        for(int i = 1; i < Integer.MAX_VALUE; i++){
+        for(int i = 1; i <= 101; i++){
             if(!list.contains(i*k))return i*k;
         }
         return 0;
